da64e8cee3da89e02e2eeb91e9d98abe3b7c0f78
Lenik <pub.lenik@bodz.net>
2004-08-08 07:22:53 +0000

    MERGED INTO USNAP

---
+++ epfs/inc/epfs.h
@@ -0,0 +1,19 @@
+
+
+#pragma once
+
+
+// practical version
+/*
+	这是EPFS的初始版，如果tit有希望，它必将被t脚本或泛g所代替。
+	This is the initial version of EPFS header, and it will be
+	replaced by T-script or g whenever possible.
+*/
+
+
+
+// M-J/D
+
+// M-J/C
+
+// M-J/I
