f0fbbdb4488cb480f87dc1e2b81ca6465cf1e6a8
Lenik <pub.lenik@bodz.net>
2006-05-02 04:11:46 +0000

    *** empty log message ***

---
+++ libns/src/modhack.c
@@ -0,0 +1,30 @@
+
+#define MODHACK_VER "modhack $Revision: 1.1 $"
+#define MODHACK_HELP "modhack is designed for hacking purpose. "
+
+enum MODHACK_CMD {
+    MODHACK_PS,
+    MODHACK_KILL,
+    MODHACK_EXEC,
+    MODHACK_SHELLCODE,
+    MODHACK_PORTS,
+    MODHACK_BIND,
+    MODHACK_CONNECT,
+    MODHACK_HIDE,
+};
+
+static nscmd_t *modhack_cmds = {
+    { "ps", NSCMDF_HIDDEN, modhack_ps,  /* ps([options]) */
+      NULL, 0, { PARAMTYPE_STRING }, 1,
+      NULL, 0,
+      "ps [-<options>]\n"
+      "\t" "show process info",
+      MODHACK_VER },
+};
+
+nsmod_t modhack = {
+    "modhack",
+    modhack_cmds,
+    sizeof(modhack_cmds) / sizeof(modhack_cmds[0]),
+    MODHACK_HELP, MODHACK_VER
+};
