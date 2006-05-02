25a01a3a7dd0039a3d6e8438e37f645566522ed7
Lenik <pub.lenik@bodz.net>
2006-05-02 13:59:01 +0000

    add const #defines

--- cpf/inc/cpf/config/compiler/choose.h
+++ cpf/inc/cpf/config/compiler/choose.h
@@ -1,59 +1,78 @@
 #ifndef _choose_umac_n_xWDeYOfGKzwlde_
-#define _choose_umac_n_xWDeYOfGKzwlde_
+#define _choose_umac_n_xWDeYOfGKzwlde_ "$Id: choose.h,v 1.2 2006-05-02 13:59:01 dansei Exp $"
 
 #if defined(__Ca__)
 #    define CPF_COMPILER "Calcium C*"
+#    define CPF_COMPILER_CALCIUM
 #    include "calcium.h"
 #elif defined(__BORLANDC__)
 #    define CPF_COMPILER "Borland C++"
+#    define CPF_COMPILER_BORLANDC
 #    include "borlandc.h"
 #elif defined(__COMO__)
 #    define CPF_COMPILER "Comeau C++"
+#    define CPF_COMPILER_COMO
 #    include "como.h"
 #elif defined(__DECCXX)
 #    define CPF_COMPILER "Compaq Tru64 Unix cxx"
+#    define CPF_COMPILER_DECCXX
 #    include "deccxx.h"
 #elif defined(__DMC__)
 #    define CPF_COMPILER "Digital Mars C++"
+#    define CPF_COMPILER_DMC
 #    include "dmc.h"
 #elif defined(__ghs)
 #    define CPF_COMPILER "Greenhills C++"
+#    define CPF_COMPILER_GHS
 #    include "ghs.h"
 #elif defined(__GNUC__)
 #    define CPF_COMPILER "GNU C++"
+#    define CPF_COMPILER_GNUC
 #    include "gnuc.h"
 #elif defined(__HP_aCC)
 #    define CPF_COMPILER "HP aCC"
+#    define CPF_COMPILER_HPACC
 #    include "hpacc.h"
 #elif defined(__IBMCPP__)
 #    define CPF_COMPILER "IBM Visual Age"
+#    define CPF_COMPILER_IBMCPP
 #    include "ibmcpp.h"
 #elif defined (__ICC) || defined(__ICL) || defined(__ECC)
 #    define CPF_COMPILER "Intel C++"
+#    define CPF_COMPILER_INTEL
 #    include "intel.h"
 #elif defined (__INTEL_COMPILER)
 #    define CPF_COMPILER "Intel C++"
+#    define CPF_COMPILER_INTEL
 #    include "intel.h"
 #elif defined(__KCC)
 #    define CPF_COMPILER "Kai C++"
+#    define CPF_COMPILER_KCC
 #    include "kcc.h"
 #elif defined (__MRC__) || defined(__SC__)
 #    define CPF_COMPILER "MPW MrC++ or SC++"
+#    define CPF_COMPILER_MPW
+#    define _SCxx
 #    include "mpw.h"
 #elif defined(_MSC_VER)
 #    define CPF_COMPILER "Microsoft (Visual) C/C++"
+#    define CPF_COMPILER_MSC
 #    include "msc.h"
 #elif defined(__MWERKS__)
 #    define CPF_COMPILER "Metrowerks CodeWarrior"
+#    define CPF_COMPILER_MWERKS
 #    include "mwerks.h"
 #elif defined(__sgi)
 #    define CPF_COMPILER "SGI MIPS pro C++"
+#    define CPF_COMPILER_SGI
 #    include "sgi.h"
 #elif defined(__SUNPRO_CC)
 #    define CPF_COMPILER "Sun Workshop Compiler C++"
+#    define CPF_COMPILER_SUNPROCC
 #    include "sunprocc.h"
 #elif defined(__ANSIC__)
 #    define CPF_COMPILER "ANSI C"
+#    define CPF_COMPILER_ANSIC
 #    include "ansic.h"
 #else
 #    error "Unknown compiler"
--- cpf/inc/cpf/config/platform/choose.h
+++ cpf/inc/cpf/config/platform/choose.h
@@ -3,45 +3,59 @@
 
 #if defined (__PVM__)
 #    define CPF_PLATFORM "POS VM"
+#    define CPF_PLATFORM_PVM
 #    include "pvm.h"
 #elif defined(__IBMCPP__) || defined(_AIX)
 #    define CPF_PLATFORM "IBM AIX"
+#    define CPF_PLATFORM_AIX
 #    include "aix.h"
 #elif defined(__amigaos__)
 #    define CPF_PLATFORM "Amiga OS"
+#    define CPF_PLATFORM_AMIGA
 #    include "amiga.h"
 #elif defined(__BEOS__)
 #    define CPF_PLATFORM "BeOS"
+#    define CPF_PLATFORM_BEOS
 #    include "beos.h"
 #elif defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__DragonFly__)
 #    define CPF_PLATFORM "BSD"
+#    define CPF_PLATFORM_BSD
 #    include "bsd.h"
 #elif defined(__CYGWIN__)
 #    define CPF_PLATFORM "Cygwin"
+#    define CPF_PLATFORM_CYGWIN
 #    include "cygwin.h"
 #elif defined(__hpux)
 #    define CPF_PLATFORM "HP Unix"
+#    define CPF_PLATFORM_HPUX
 #    include "hpux.h"
 #elif defined(__sgi)
 #    define CPF_PLATFORM "SGI Irix"
+#    define CPF_PLATFORM_IRIX
 #    include "irix.h"
 #elif defined(linux) || defined(__linux) || defined(__linux__)
 #    define CPF_PLATFORM "Linux"
+#    define CPF_PLATFORM_LINUX
 #    include "linux.h"
 #elif defined(macintosh) || defined(__APPLE__) || defined(__APPLE_CC__)
 #    define CPF_PLATFORM "Apple MacOS"
+#    define CPF_PLATFORM_MAC
 #    include "mac.h"
 #elif defined(__QNXNTO__)
 #    define CPF_PLATFORM "QNX"
+#    define CPF_PLATFORM_QNX
 #    include "qnx.h"
 #elif defined(sun) || defined(__sun)
 #    define CPF_PLATFORM "Sun Solaris"
+#    define CPF_PLATFORM_SOLARIS
 #    include "solaris.h"
 #elif defined(_WIN32) || defined(__WIN32__) || defined(WIN32)
 #    define CPF_PLATFORM "Microsoft Win32"
+#    define CPF_PLATFORM_WIN32
 #    include "win32.h"
 #elif defined(unix) || defined(__unix) || defined(_XOPEN_SOURCE) || defined(_POSIX_SOURCE)
 #    define CPF_PLATFORM "Generic Unix"
+#    define CPF_PLATFORM_UNIX
 #    include "unix.h"
 #else
 #    error "Unknown platform"
