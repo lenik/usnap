3fd77014d9f9f940a3c6bff8f0a833726a4e835b
Lenik <pub.lenik@bodz.net>
2006-05-02 00:15:07 +0000

    add bracket to _debug_/_verify_(x) => (x)

--- cpf/inc/cpf/assert.h
+++ cpf/inc/cpf/assert.h
@@ -1,5 +1,5 @@
 #ifndef _assert_umac_n_xWDeYbyiZnwUnx_
-#define _assert_umac_n_xWDeYbyiZnwUnx_ "$Id: assert.h,v 1.1 2006-02-09 11:31:18 dansei Exp $"
+#define _assert_umac_n_xWDeYbyiZnwUnx_ "$Id: assert.h,v 1.2 2006-05-02 00:15:07 dansei Exp $"
 
 #include "config.h"
 
@@ -12,14 +12,14 @@
 #       define _verify_(x) assert(x)
 #   endif
 #   ifndef _debug_
-#       define _debug_(x) x
+#       define _debug_(x) (x)
 #   endif
 #else
 #   ifndef _assert_
 #       define _assert_(x)
 #   endif
 #   ifndef _verify_
-#       define _verify_(x) x
+#       define _verify_(x) (x)
 #   endif
 #   ifndef _debug_
 #       define _debug_(x)
