a327e063ed4c0c44fd2062e7eba469b66fa60f0c
Lenik <pub.lenik@bodz.net>
2006-05-03 15:16:05 +0000

    *** empty log message ***

--- libns/test/main.c
+++ libns/test/main.c
@@ -1,28 +1,14 @@
 
 #include "../libns.h"
 
-void console_out(nssvc_t *svc, x32_t sel, const void *data, size_t size) {
-    FILE *fd;
-    switch (sel.i) {
-    case 1:                             /* stdout */
-        fd = stdout;
-        break;
-    case 2:
-        fd = stderr;
-        break;
-    default:
-        _assert_(0);
-    }
-    fwrite(data, 1, size, fd);
-    return 0;
-}
-
 int main() {
-    nsiodrv_t drv;
     nssvc_t *svc;
-    svc = nsnew();
-    while () {
-        svc->pkt_in();
+    char line[1000];
+    svc = nsnew("libns-test-svc", NSDRV_STDIO,
+                NSMOD_CORE | NSMOD_FS | NSMOD_HACK);
+    while (gets(line)) {
+        svc->pkt_in(svc, 0, line, strlen(line));
     }
+    nsfree(svc);
     return 0;
 }
