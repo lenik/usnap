4070dba547848d361deb69ff15e09a1da233ed19
Lenik <pub.lenik@bodz.net>
2006-05-13 15:30:18 +0000

    initial

---
+++ cpf/config/stdhdrs.h
@@ -0,0 +1,61 @@
+#ifndef _stdhdrs_utim_ddT3F0l44OzFe1hn_
+#define _stdhdrs_utim_ddT3F0l44OzFe1hn_ 1
+
+#include "config.h"
+
+/* do real include */
+
+#include <stdio.h>
+#include <stdarg.h>
+
+#if HAVE_SYS_TYPES_H
+# include <sys/types.h>
+#endif
+#if HAVE_SYS_STAT_H
+# include <sys/stat.h>
+#endif
+
+#if STDC_HEADERS
+# include <stdlib.h>
+# include <stddef.h>
+#else
+# if HAVE_STDLIB_H
+#  include <stdlib.h>
+# endif
+#endif
+
+#if HAVE_STRING_H
+# if !STDC_HEADERS && HAVE_MEMORY_H
+#  include <memory.h>
+# endif
+# include <string.h>
+#endif
+
+#if HAVE_STRINGS_H
+# include <strings.h>
+#endif
+
+#if HAVE_INTTYPES_H
+# include <inttypes.h>
+#else
+# if HAVE_STDINT_H
+#  include <stdint.h>
+# endif
+#endif
+
+#if HAVE_UNISTD_H
+# include <unistd.h>
+#endif
+
+#if TIME_WITH_SYS_TIME
+# include <sys/time.h>
+# include <time.h>
+#else
+# if HAVE_SYS_TIME_H
+#  include <sys/time.h>
+# else
+#  include <time.h>
+# endif
+#endif
+
+#endif
