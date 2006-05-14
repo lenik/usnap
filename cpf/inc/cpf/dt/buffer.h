3fc3ecfe9c58df24790e0d5f265ce4fc6036987f
Lenik <pub.lenik@bodz.net>
2006-05-14 04:58:29 +0000

    add const modifiers

--- cpf/inc/cpf/dt/buffer.h
+++ cpf/inc/cpf/dt/buffer.h
@@ -1,5 +1,5 @@
 #ifndef _buffer_utim_c7Q9SKNdrPidPwJP_
-#define _buffer_utim_c7Q9SKNdrPidPwJP_ "$Id: buffer.h,v 1.2 2006-05-13 15:29:16 dansei Exp $"
+#define _buffer_utim_c7Q9SKNdrPidPwJP_ "$Id: buffer.h,v 1.3 2006-05-14 04:58:29 dansei Exp $"
 
 #include "raw.h"
 
@@ -14,7 +14,7 @@ struct _buffer_t {
 
 buffer_t *buffer_new(void *data, size_t size, size_t alloc, size_t growth);
 
-buffer_t *buffer_append(buffer_t *buffer, void *data, size_t size);
+buffer_t *buffer_append(buffer_t *buffer, const void *data, size_t size);
 
 #define buffer_append_val(buf, t, v) \
     (*(t *)buffer_dataend(buffer_append((buf), 0, sizeof(t)), \
@@ -23,7 +23,7 @@ buffer_t *buffer_append(buffer_t *buffer, void *data, size_t size);
 
 void *buffer_dataend(buffer_t *buffer, int offset);
 
-buffer_t *buffer_prepend(buffer_t *buffer, void *data, size_t size);
+buffer_t *buffer_prepend(buffer_t *buffer, const void *data, size_t size);
 
 #define buffer_prepend_val(buf, t, v) \
     (*(t *)(buffer_prepend((buf), 0, sizeof(t))->data) \
