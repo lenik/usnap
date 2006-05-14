7e7f94e72163bfa6a8ba4e3ab5546870660b3b57
Lenik <pub.lenik@bodz.net>
2006-05-14 13:10:12 +0000

    code format

--- cpf/inc/cpf/dt/buffer.h
+++ cpf/inc/cpf/dt/buffer.h
@@ -1,5 +1,5 @@
 #ifndef _buffer_utim_c7Q9SKNdrPidPwJP_
-#define _buffer_utim_c7Q9SKNdrPidPwJP_ "$Id: buffer.h,v 1.3 2006-05-14 04:58:29 dansei Exp $"
+#define _buffer_utim_c7Q9SKNdrPidPwJP_ "$Id: buffer.h,v 1.4 2006-05-14 13:10:12 lenik Exp $"
 
 #include "raw.h"
 
@@ -13,22 +13,20 @@ struct _buffer_t {
 };
 
 buffer_t *buffer_new(void *data, size_t size, size_t alloc, size_t growth);
-
 buffer_t *buffer_append(buffer_t *buffer, const void *data, size_t size);
+buffer_t *buffer_prepend(buffer_t *buffer, const void *data, size_t size);
+void *buffer_free(buffer_t *buffer, int free_data);
 
-#define buffer_append_val(buf, t, v) \
+void *buffer_data(buffer_t *buffer, size_t offset);
+void *buffer_dataend(buffer_t *buffer, int offset);
+
+#define buffer_appendv(buf, t, v) \
     (*(t *)buffer_dataend(buffer_append((buf), 0, sizeof(t)), \
                           -sizeof(t)) \
         = (t)(v))
 
-void *buffer_dataend(buffer_t *buffer, int offset);
-
-buffer_t *buffer_prepend(buffer_t *buffer, const void *data, size_t size);
-
-#define buffer_prepend_val(buf, t, v) \
+#define buffer_prependv(buf, t, v) \
     (*(t *)(buffer_prepend((buf), 0, sizeof(t))->data) \
         = (t)(v))
-
-void *buffer_free(buffer_t *buffer, int free_data);
 
 #endif
--- cpf/src/dt/buffer.c
+++ cpf/src/dt/buffer.c
@@ -3,7 +3,8 @@
 #include <cpf/assert.h>
 #include <cpf/dt/buffer.h>
 
-buffer_t *buffer_new(void *data, size_t size, size_t alloc, size_t growth) {
+buffer_t *
+buffer_new(void *data, size_t size, size_t alloc, size_t growth) {
     buffer_t *buffer;
     buffer = (buffer_t *)malloc(sizeof(buffer_t));
 
@@ -23,7 +24,8 @@ buffer_t *buffer_new(void *data, size_t size, size_t alloc, size_t growth) {
     return buffer;
 }
 
-buffer_t *buffer_append(buffer_t *buffer, void *data, size_t size) {
+buffer_t *
+buffer_append(buffer_t *buffer, void *data, size_t size) {
     if (! buffer) {
         buffer = buffer_new(data, size, size, 0);
         return buffer;
@@ -38,17 +40,20 @@ buffer_t *buffer_append(buffer_t *buffer, void *data, size_t size) {
     return buffer;
 }
 
-void *buffer_dataend(buffer_t *buffer, int offset) {
+void *
+buffer_dataend(buffer_t *buffer, int offset) {
     _assert_(buffer);
     _assert_(offset <= 0 && offset >= -(int)buffer->size);
     return ((u8_t *)buffer->data) + buffer->size + offset;
 }
 
-buffer_t *buffer_prepend(buffer_t *buffer, void *data, size_t size) {
+buffer_t *
+buffer_prepend(buffer_t *buffer, void *data, size_t size) {
     return buffer;
 }
 
-void *buffer_free(buffer_t *buffer, int free_data) {
+void *
+buffer_free(buffer_t *buffer, int free_data) {
     void *data;
     if (! buffer)
         return 0;
