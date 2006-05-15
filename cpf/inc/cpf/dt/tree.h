81ef2e00c2d1e98d88107579afab4c35b788911f
Lenik <pub.lenik@bodz.net>
2006-05-15 10:28:52 +0000

    compact coding style

--- cpf/inc/cpf/dt/tree.h
+++ cpf/inc/cpf/dt/tree.h
@@ -1,6 +1,9 @@
 #ifndef _tree_umac_n_xWDeYI1ZfiVn5P_
 #define _tree_umac_n_xWDeYI1ZfiVn5P_
 
+#include <cpf/api.h>
+#include "raw.h"
+
 typedef struct _node_t node_t;
 struct _node_t {
     guint16 type;
@@ -30,105 +33,42 @@ struct _node_t {
 #  define NODE_CHECK_REMOVE(r)  G_STMT_START { (void)0; } G_STMT_END
 #endif
 
-extern gboolean (*node_check_failure)(const char *, node_t *, node_t *);
+extern gboolean (_cc *node_check_failure)(const char *, node_t *, node_t *);
 
-gboolean
-_node_check_node(flat_t *flat, node_t *start, gboolean upwards);
+gboolean _cc node_check_node(flat_t *flat, node_t *start, gboolean upwards);
+gboolean _cc node_check_node(flat_t *flat, node_t *node);
+gboolean _cc node_check_add(flat_t *flat, node_t *add);
+gboolean _cc node_check_remove(flat_t *flat, node_t *remove);
+flat_t * _cc node_flat(node_t *node);   /* build if not exist */
 
-gboolean
-node_check_node(flat_t *flat, node_t *node);
+node_t * _cc node_new(node_t *parent, int type, const char *text);
+void     _cc node_init(node_t *node, node_t *parent, int type, const char *text);
+void     _cc node_free(node_t *node);
+void     _cc node_free_recursive(node_t *node);
+void     _cc node_detach(node_t *node);
+node_t * _cc node_swap_children(node_t *node); /* return the first child */
+int      _cc node_type(node_t *node);
+const char * _cc node_typename(node_t *node);
+const char * _cc node_text(node_t *node);
+void     _cc node_set_text(node_t *node, const char *text);
+node_t * _cc node_prev(node_t *node);
+node_t * _cc node_set_prev(node_t *node, node_t *prev);
+node_t * _cc node_next(node_t *node);
+node_t * _cc node_set_next(node_t *node, node_t *next);
+node_t * _cc node_prev_type(node_t *node, int node_type);
+node_t * _cc node_next_type(node_t *node, int node_type);
+node_t * _cc node_first(node_t *node);
+node_t * _cc node_last(node_t *node);
+void     _cc node_insert_prev(node_t *node, node_t *prev);
+void     _cc node_insert_next(node_t *node, node_t *next);
+node_t * _cc node_root(node_t *node);
+node_t * _cc node_parent(node_t *node);
+node_t * _cc node_set_parent(node_t *node, node_t *parent);
+node_t * _cc node_child(node_t *node);
+node_t * _cc node_set_child(node_t *node, node_t *child);
+void     _cc node_remove_children(node_t *node);
 
-gboolean
-node_check_add(flat_t *flat, node_t *add);
-
-gboolean
-node_check_remove(flat_t *flat, node_t *remove);
-
-flat_t *                        /* build if not exist */
-node_flat(node_t *node);
-
-node_t *
-node_new(node_t *parent, int type, const char *text);
-
-void
-node_init(node_t *node, node_t *parent, int type, const char *text);
-
-void
-node_free(node_t *node);
-
-void
-node_free_recursive(node_t *node);
-
-void
-node_detach(node_t *node);
-
-node_t *                        /* return the first child */
-node_swap_children(node_t *node);
-
-int
-node_type(node_t *node);
-
-const char *
-node_typename(node_t *node);
-
-const char *
-node_text(node_t *node);
-
-void
-node_set_text(node_t *node, const char *text);
-
-node_t *
-node_prev(node_t *node);
-
-node_t *
-node_set_prev(node_t *node, node_t *prev);
-
-node_t *
-node_next(node_t *node);
-
-node_t *
-node_set_next(node_t *node, node_t *next);
-
-node_t *
-node_prev_type(node_t *node, int node_type);
-
-node_t *
-node_next_type(node_t *node, int node_type);
-
-node_t *
-node_first(node_t *node);
-
-node_t *
-node_last(node_t *node);
-
-void
-node_insert_prev(node_t *node, node_t *prev);
-
-void
-node_insert_next(node_t *node, node_t *next);
-
-node_t *
-node_root(node_t *node);
-
-node_t *
-node_parent(node_t *node);
-
-node_t *
-node_set_parent(node_t *node, node_t *parent);
-
-node_t *
-node_child(node_t *node);
-
-node_t *
-node_set_child(node_t *node, node_t *child);
-
-void
-node_remove_children(node_t *node);
-
-int
-node_breadth(node_t *node);
-
-int
-node_depth(node_t *node);
+int      _cc node_breadth(node_t *node);
+int      _cc node_depth(node_t *node);
 
 #endif
