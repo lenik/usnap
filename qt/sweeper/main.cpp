c1ffff5ffd0343c460c8abd80c28aafe248cae7a
Lenik <pub.lenik@bodz.net>
2012-06-27 08:15:09 +0800

    include "session.h" in main.

--- qt/sweeper/main.cpp
+++ qt/sweeper/main.cpp
@@ -1,6 +1,8 @@
 #include <QtGui/QApplication>
 #include "qmlapplicationviewer.h"
 
+#include "session.h"
+
 Q_DECL_EXPORT int main(int argc, char *argv[])
 {
     QScopedPointer<QApplication> app(createApplication(argc, argv));
